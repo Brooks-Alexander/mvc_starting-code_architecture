05317adde3eb71189e907e61ea901eb3750a9b5f
Brooks-Alexander <brooks.alexander.r@gmail.com>
2025-07-28 11:37:34 +0200

    router model expense.js connected with controllers middleware callback functions to update server information through application middleware to an instance of the router object by using the router.express() and router.METHOD() functions, where METHOD is the HTTP method of the request that the middleware function handles (such as GET, PUT, or POST) in lowercase.

--- routes/expense.js
+++ routes/expense.js
@@ -1,18 +1,20 @@
 // require express and it's router component
 const express = require('express');
-
+const { create, expenseByDate, expenseById, read, update } = require('../controllers')
 const router = express.Router();
 
 // require the middlewares and callback functions from the controller directory
 
 // Create POST route to create an expense
+router.post('/expense/create', create);
 
 // Create GET route to read an expense
-
+router.get('/expense/:id', expenseById, read);
 // Create PUT route to update an expense
-
+router.put('/expense/update/:id', expenseById, update)
 // Create DELETE route to remove an expense
-
+router.delete('/expense/remove/:id', remove);
 // Create GET route to read a list of expenses
+router.get('/expense/:date', expenseByDate, read);
 
 module.exports = router;
